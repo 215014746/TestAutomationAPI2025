b56a44e44f194f58929937bffedbdd68f3936fc3
Zinhle Ndubane <ndubanepz@gmail.com>
2026-01-17 21:59:50 +0200

    modified testimonials clas

---
+++ src/test/java/Basic/Testimonials.java
@@ -0,0 +1,33 @@
+package Basic;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.post;
+
+public class Testimonials {
+    @Test
+    public void loginUserTest() {
+       String baseURL = "https://www.ndosiautomation.co.za/";
+       String path = "/API/login";
+       String payload = "" +
+               "{\n" +
+               "  \"email\": \"john.doe@example444.com\",\n" +
+               "  \"password\": \"SecurePass123!\"\n" +
+               "}";
+
+        Response responseBody = RestAssured.given()
+                .baseUri(baseURL)
+                .basePath(path)
+                .header("Content-Type", "application/json")
+                .body(payload)
+                .log().all()
+                .post().prettyPeek();
+
+
+        int actualStatusCode = responseBody.getStatusCode();
+        Assert.assertEquals(actualStatusCode, 200, "Status code should be 200");
+    }
+}
